9c17d413f6f27073e3389858c80bae35bfa71d09
dktrotti <dktrotti@ualberta.ca>
2015-09-14 18:31:52 -0600

    Added multiplayer skeleton

--- app/src/main/java/dktrotti/ece301assignment1/MultiplayerActivity.java
+++ app/src/main/java/dktrotti/ece301assignment1/MultiplayerActivity.java
@@ -1,24 +1,22 @@
 package dktrotti.ece301assignment1;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MultiplayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_multiplayer);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_multiplayer, menu);
         return true;
     }
 
@@ -35,15 +33,5 @@ public class MainMenuActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onSingleplayerbuttonClick(View view) {
-        Intent intent = new Intent(this, SingleplayerActivity.class);
-        startActivity(intent);
-    }
-
-    public void onMultiplayerbuttonClick(View view) {
-        Intent intent = new Intent(this, PlayerSelectActivity.class);
-        startActivity(intent);
     }
 }
